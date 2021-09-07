ed6d02a10f5e78d6ccfc83790ae8e6a359ea3890
marcussen <mark.parellada@gmail.com>
2021-09-07 17:30:13 +0200

    proves

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { arrow, Dog } from './module-a';
+
+arrow();
+const orelletes = new Dog('orelletes');
+console.log(orelletes.sayHi());
---
+++ src/module-a.js
@@ -0,0 +1,16 @@
+export const arrow = () => {
+    let variable = 'canvi let';
+    console.log('la funció fletxa rutlla');
+    variable = 'variable let canviada';
+    console.log(variable);
+  };
+  
+  export class Dog {
+    constructor(name) {
+      this.name = name;
+    }   
+  
+    sayHi() {
+      return `${this.name} say bub bub!`;
+    }
+  }
